57dbc6b5331aff2b3ae3104be4de3a1785e09b7f
Ayush-iyengar <ayushiyengar2007@gmail.com>
2024-12-28 18:32:10 -0800

    zephyrus site plutonium template

---
+++ codespaces/components/Features.js
@@ -0,0 +1,208 @@
+export default function Features() {
+  return (
+    <section id="features" className="py-12">
+      <div className="max-w-xl px-4 py-12 mx-auto sm:px-6 lg:max-w-6xl lg:px-8">
+        <h1 className="mb-8 text-2xl font-bold tracking-normal text-center text-gray-800 md:leading-tight md:tracking-normal dark:text-gray-200 md:text-4xl">
+          Supporting the{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r dark:bg-gradient-to-r from-offwhite via-vanilla to-uranianBlue dark:from-offwhite dark:via-vanilla dark:to-uranianBlue">
+            finest
+          </span>{" "}
+          and{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r dark:bg-gradient-to-r from-offwhite via-vanilla to-uranianBlue dark:from-offwhite dark:via-vanilla dark:to-uranianBlue">
+            richest
+          </span>{" "}
+          features.
+        </h1>
+        <p className="max-w-md mx-auto mb-10 text-lg text-vanilla dark:text-vanilla md:text-lg">
+          We&apos;re introducing a new way to track AQI and receive protection at the same time!
+        </p>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <div className="mb-10 space-y-6">
+            <h1 className="text-xl font-bold text-center text-davysgray dark:text-davysgray md:text-2xl sm:text-left">
+              GNNs
+            </h1>
+            <div className="h-auto">
+              <a
+                href="https://unsplash.com/photos/ymVslcVAzg8"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  className="transition duration-700 rounded shadow-2xl h-80 hover:shadow-3xl md:hover:transform md:hover:scale-105"
+                  src="/images/placeholder-2.webp"
+                  alt="Placeholder for Next.js"
+                />
+              </a>
+            </div>
+            <p className="mx-2 font-normal text-vanilla dark:text-vanilla text-md">
+              <ul className="mr-0 md:mr-10">
+                <li className="mb-2">
+                  <span className="font-semibold">Overview</span>:Geospatial neural networks (GNNs) are AI models 
+                  focused on analyzing spatial data and solving location-based problems 
+                  in mapping, logistics, and urban planning.
+                </li>
+                <li className="mb-2">
+                  <span className="font-semibold">Improved Performance</span>:
+                  GNNs are a cost-effective method of creating a denser AQI grid to guarantee in-depth air quality analysis of every community
+                </li>
+                <li className="mb-2">
+                  <span className="text-vanilla dark:text-vanilla px-1 py-0.75 border border-davysgray dark:border-davysgray rounded-md bg-uranianBlue dark:bg-uranianBlue">
+                    
+                  </span>{" "}
+                  
+                </li>
+                <li className="mb-2">
+                  <span className="text-vanilla">
+                    
+                  </span>{" "}
+                  
+                </li>
+              </ul>
+            </p>
+          </div>
+          <div className="mb-10 space-y-6">
+            <h1 className="text-davysgray">
+              Infrared Spectroscopy Sensors
+            </h1>
+            <div className="h-auto">
+              <a
+                href="https://unsplash.com/photos/qOEiV-8w-MQ"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  className="transition duration-700 rounded shadow-2xl h-80 hover:shadow-3xl md:hover:transform md:hover:scale-105"
+                  src="/images/placeholder-3.webp"
+                  alt="Placeholder for TailwindCSS JIT"
+                />
+              </a>
+            </div>
+            <p className="mx-2 font-normal text-vanilla dark:text-vanilla text-md">
+              <ul className="mr-0 md:mr-10">
+                <li className="mb-2">
+                  <span className="font-semibold"></span>: A
+                  faster, more thorough particle detecion system
+                  v2.1+.
+                </li>
+                <li className="mb-2">
+                  <span className="font-semibold">
+                    Replaces clunky particle sensors
+                  </span>
+                </li>
+                <li className="mb-2">
+                  <span className="font-semibold">
+                    1 sensor for every 3 particle sensors, one device analyzes all types of pollutant
+                  </span>
+                </li>
+                <li className="mb-2">
+                  <span className="font-semibold">
+                    A better, more effective performance in detection
+                  </span>
+                </li>
+                <li className="mb-2">
+                  <span className="text-p dark:text-purple-400 px-1 py-0.75 border border-gray-200 dark:border-gray-800 rounded-md bg-uranianBlue dark:bg-uranianBlue">
+                    
+                  </span>
+                </li>
+              </ul>
+            </p>
+          </div>
+          <div className="mb-10 space-y-6">
+            <h1 className="text-xl font-bold text-center text-davysgray dark:text-davysgray md:text-2xl sm:text-left">
+              Cutting-Edge Tech
+            </h1>
+            <div className="h-auto">
+              <a
+                href="https://unsplash.com/photos/p7o0qrl8hv8"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  className="transition duration-700 rounded shadow-2xl h-80 hover:shadow-3xl md:hover:transform md:hover:scale-105"
+                  src="/images/placeholder-4.webp"
+                  alt="Placeholder for Dark Mode"
+                />
+              </a>
+            </div>
+            <p className="mx-2 font-normal text-vanilla dark:text-vanilla text-md">
+              <ul className="mr-0 md:mr-10">
+                <li className="mb-2">
+                  <a
+                    href="https://github.com/pacocoursey/next-themes"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="font-semibold"
+                  >
+                    Exponential Improvement
+                  </a>
+                  The NVIDIA A100 GPU allows us to train our model until it surpasses perfection
+                </li>
+                <li className="mb-2">
+                  <span className="font-semibold">Systems</span>: Uses
+                  state-of-the-art technology to guarantee up to date tracking
+                </li>
+                <li className="mb-2">
+                  <span className="font-semibold">AWS Sagemaker</span>: The management of hundreds of GNNs runs 
+                  through the most powerful machine learning platforms and is 
+                </li>
+                <li className="mb-2">
+                  <span className="text-purple-500 dark:text-purple-400 px-1 py-0.75 border border-gray-200 dark:border-gray-800 rounded-md bg-uranianBlue dark:bg-uranianBlue">
+                    ``
+                  </span>{" "}
+                  
+                </li>
+              </ul>
+            </p>
+          </div>
+          <div className="mb-10 space-y-6">
+            <h1 className="text-xl font-bold text-center text-davysgray dark:text-davysgray md:text-2xl sm:text-left">
+              CNNs
+            </h1>
+            <div className="h-auto">
+              <a
+                href="https://unsplash.com/photos/_CrD1UmfWqc"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  className="transition duration-700 rounded shadow-2xl h-80 hover:shadow-3xl md:hover:transform md:hover:scale-105"
+                  src="/images/placeholder-5.webp"
+                  alt="Placeholder for Next-SEO"
+                />
+              </a>
+            </div>
+            <p className="mx-2 font-normal text-vanilla dark:text-vanilla text-md">
+              <ul className="mr-0 md:mr-10">
+                <li className="mb-2">
+                  <a
+                    href="https://github.com/garmeeh/next-seo"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="font-semibold"
+                  >
+                    Convolutional Neural Networks
+                  </a>
+                  : cutting-edge AI architectures designed to efficiently process and analyze visual data, 
+                  powering advancements in image recognition and computer vision.
+                </li>
+                <li className="mb-2">
+                  <span className="font-semibold">R</span>: 
+                  <span className="text-vanilla dark:text-vanilla px-1 py-0.75 border border-gray-200 dark:border-gray-800 rounded-md bg-uranianBlue dark:bg-uranianBlue">
+                    ``
+                  </span>{" "}
+                  <span className="text-vanilla dark:text-purple-400 px-1 py-0.75 border border-gray-200 dark:border-gray-800 rounded-md bg-uranianBlue dark:bg-uranianBlue">
+                    {}
+                  </span>{" "}
+                </li>
+                <li className="mb-2">
+                  <span className="font-semibold">Data Optimization</span>: CNNs allow Zephyrus to predict future AQI trends in 
+                  every community, allowing filtration companies to efficiently manage their expenditures
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ codespaces/components/Sponsors.js
@@ -0,0 +1,67 @@
+export default function Sponsors() {
+  return (
+    <section className="pb-10" id="sponsors">
+      <div className="px-4 pb-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <h1 className="mb-8 text-2xl font-bold tracking-normal text-center text-darkvanilla md:leading-tight md:tracking-normal dark:text-darkvanilla md:text-4xl">
+           Connecting filtration distributors with low-income communities
+        </h1>
+        <p className="max-w-md mx-auto mb-12 text-lg text-vanilla dark:text-vanilla text-left md:text-center md:text-lg">
+          <b></b> A one-of-a-kind AI model that uses geospatial neural networks
+          in tandem with infrared spectroscopy sensors to create
+          a denser AQI grid, ensuring no community is left unnoticed
+          View the demo below!
+        </p>
+        <div className="grid grid-cols-2 gap-8 mt-6 md:grid-cols-4">
+          <div className="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
+            <svg
+              
+            >
+              <path fill="currentColor" d="M0 0h200v200H0z" />
+              <text
+               
+              >
+                nd
+              </text>
+            </svg>
+          </div>
+          <div className="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
+            <svg
+              
+            >
+              <path fill="currentColor" d="M0 0h200v200H0z" />
+              <text
+                
+              >
+                nd
+              </text>
+            </svg>
+          </div>
+          <div className="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
+            <svg
+              
+            >
+              <path fill="currentColor" d="M0 0h200v200H0z" />
+              <text
+              
+              >
+                nd
+              </text>
+            </svg>
+          </div>
+          <div className="flex justify-center col-span-1 md:col-span-2 lg:col-span-1">
+            <svg
+              
+            >
+              <path fill="currentColor" d="M0 0h200v200H0z" />
+              <text
+               
+              >
+                nd
+              </text>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
