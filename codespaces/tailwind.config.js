789ba832fdc7bddd4c0789dbc309dffb1b6df514
Ayush-iyengar <ayushiyengar2007@gmail.com>
2024-12-29 23:16:04 -0800

    more updates

---
+++ codespaces/tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'false'
+  theme: {
+    extend: {
+      colors: {
+        sky: colors.sky,
+        cyan: colors.cyan,
+        pink: colors.pink,
+        rose: colors.rose,
+        teal: colors.teal,
+        fuchsia: colors.fuchsia,
+        uranianBlue: "#AFDBF5", // Adding Uranian Blue
+        vanilla: "#F3E5AB", // Adding Vanilla
+        davysgray:'#555555', // Davy's Gray color code
+        offwhite:'#f8f8f8', // Off White color code
+        darkvanilla:'#d1bea8', // Dark Vanilla color code
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
